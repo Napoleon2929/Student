7f4684d8a51836cfdebaf19bc6ad648007cd5109
Napoleon2929 <52378992+Napoleon2929@users.noreply.github.com>
2019-12-11 20:47:10 +0300

    Add save settings

---
+++ StudentBook/StudentBook/IParams.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace StudentBook
+{
+    public interface IParams
+    {
+        Parametrs GetParametrs();
+        void SetParametrs(Parametrs parametrs);
+    }
+}
--- StudentBook/StudentBook/Settings.xaml.cs
+++ StudentBook/StudentBook/Settings.xaml.cs
@@ -12,10 +12,22 @@ namespace StudentBook
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Settings : ContentPage
     {
+        private Parametrs parametrs;
+        private IParams iParams = DependencyService.Get<IParams>();
         public Settings()
         {
             InitializeComponent();
-            
+            parametrs = iParams.GetParametrs();
+            Sounds.IsToggled = parametrs.Sounds;
+            Notices.IsToggled = parametrs.Notices;
+            Disappearing += Settings_Disappearing;
+        }
+
+        private void Settings_Disappearing(object sender, EventArgs e)
+        {
+            parametrs.Sounds = Sounds.IsToggled;
+            parametrs.Notices = Notices.IsToggled;
+            iParams.SetParametrs(parametrs);
         }
         private async void SelectSubject(object sender, EventArgs e)
         {
